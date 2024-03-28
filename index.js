d8e8beb81219c55709ffac63704605dafb2c4d14
sabeerbikba <sabeerbikba02@gmail.com>
2024-03-28 21:08:22 +0530

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'error.html'));
+});
+
+app.listen(3005);
