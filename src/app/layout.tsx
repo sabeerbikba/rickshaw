8685756db099a794efbd2edaa5d6910356bd845f
sabeerbikba <sabeerbikba02@gmail.com>
2024-10-28 00:39:12 +0530

    Remove GTM code from RootLayout as it was not functioning properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,7 @@ import Line from "@/components/home/line";
 import UploadModal from "@/components/gallery/uploadmodal";
 import GalleryScroll from "@/components/gallery/galleryscroll";
 import Class from "@/components/home/class";
-import Script from 'next/script';
 import "./globals.css";
-import { BASE_URL } from '@/data/envimports';
 
 export const viewport: Viewport = {
    width: 'device-width',
@@ -27,64 +25,35 @@ export const metadata: Metadata = {
    },
 };
 
-const RootLayout: FC<Readonly<{ children: React.ReactNode; }>> = ({ children }):
-   JSX.Element => {
-
-   const GTM_ID = 'GTM-P52WR3CZ';
-
-   return (
-      <html lang="en">
-         <head>
-            <meta name="twitter:url" content={BASE_URL} />
-            <Script
-               id="gtm-script"
-               strategy="afterInteractive"
-               dangerouslySetInnerHTML={{
-                  __html: `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');
-`
-               }}
-            />
-         </head>
-         <body>
-            <header className="header">
-               <div className="nav-nav-nav">
-                  <div className="nav flex">
-                     <div className="nav-logo-div">
-                        <Link className="text-dec-none" href="/">
-                           <Logo />
-                        </Link>
-                     </div>
-                     <div className="nav-nav-div">
-                        <nav className="nav-links">
-                           <ul className="nav-ul">
-                              <NavLinks />
-                           </ul>
-                        </nav>
-                        <Line />
-                     </div>
+const RootLayout: FC<{ children: React.ReactNode; }> = ({ children }):
+   JSX.Element => (
+   <html lang="en">
+      <body>
+         <header className="header">
+            <div className="nav-nav-nav">
+               <div className="nav flex">
+                  <div className="nav-logo-div">
+                     <Link className="text-dec-none" href="/">
+                        <Logo />
+                     </Link>
+                  </div>
+                  <div className="nav-nav-div">
+                     <nav className="nav-links">
+                        <ul className="nav-ul">
+                           <NavLinks />
+                        </ul>
+                     </nav>
+                     <Line />
                   </div>
                </div>
-            </header>
-            {children}
-            <UploadModal />
-            <GalleryScroll />
-            <Class />
-            <noscript>
-               <iframe
-                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-                  height="0"
-                  width="0"
-                  style={{ display: 'none', visibility: 'hidden' }}
-               />
-            </noscript>
-         </body>
-      </html>
-   );
-};
+            </div>
+         </header>
+         {children}
+         <UploadModal />
+         <GalleryScroll />
+         <Class />
+      </body>
+   </html>
+);
 
 export default RootLayout;
